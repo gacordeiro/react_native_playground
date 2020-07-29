bc1ed0e6d25be198698097c5de2008f386296f72
Guilherme Cordeiro <guilherme.cordeiro@luizalabs.com>
2020-07-29 17:57:05 -0300

    ass counter screen scaffolding

---
+++ rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const CounterScreen = () => {
+  return (
+      <View>
+        <Text> Current Count: </Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
