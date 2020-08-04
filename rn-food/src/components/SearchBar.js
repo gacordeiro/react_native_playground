3e217aa4d54330b021f4902a031efbfbb5219f3d
Guilherme Cordeiro <guilherme.cordeiro@luizalabs.com>
2020-08-04 16:30:58 -0300

    setting up searchbar state

--- rn-food/src/components/SearchBar.js
+++ rn-food/src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
   return (
       <View style={styles.backgroundStyle}>
         <Feather name="search" style={styles.iconStyle}/>
-        <TextInput style={styles.inputStyle}/>
+        <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle}
+            placeholder="Search"
+            value={term}
+            onChangeText={onTermChange}
+            onEndEditing={onTermSubmit}
+        />
       </View>
   );
 };
--- rn-food/src/screens/SearchScreen.js
+++ rn-food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import SearchBar from '../components/SearchBar';
 
@@ -9,10 +9,15 @@ import SearchBar from '../components/SearchBar';
 // g28ntXbEfpypw9s-vHz9_E3ywiHuphODFxSlWzRx6UHfdxGF0OD_M7bJkihif_A6Busmljz84Zm0SOruf62oyAgVvXuWl76rVIBI5aJgnTPHbq42fbtuj5I9KH0oX3Yx
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
   return (
       <View>
-        <SearchBar/>
-        <Text>Search Screen</Text>
+        <SearchBar
+            term={term}
+            onTermChange={setTerm}
+            onTermSubmit={() => console.log('submitted')}
+        />
+        <Text>Search Screen: {term}</Text>
       </View>
   );
 };
