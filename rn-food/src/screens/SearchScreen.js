74700c452426a0971cecdf23d76008a6dbf4a39d
Guilherme Cordeiro <guilherme.cordeiro@luizalabs.com>
2020-08-05 10:33:01 -0300

    useEffect hook

--- rn-food/src/screens/SearchScreen.js
+++ rn-food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,13 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
+    console.log(`search for: ${searchTerm}`);
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term: term,
+          term: searchTerm,
           location: 'san jose',
         },
       });
@@ -25,12 +26,14 @@ const SearchScreen = () => {
     }
   };
 
+  useEffect(() => { searchApi('jumanji'); }, []);
+
   return (
       <View>
         <SearchBar
             term={term}
             onTermChange={setTerm}
-            onTermSubmit={searchApi}
+            onTermSubmit={() => searchApi(term)}
         />
         {errorMessage
             ? <Text style={styles.error}>{errorMessage}</Text>
